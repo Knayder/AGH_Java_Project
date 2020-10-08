fcf8d1933c9ae78d5ba33e4a94ca2bad3b241157
Knayder <tomaszjatkowski42@gmail.com>
2020-10-08 12:37:04 +0200

    init

---
+++ src/agh/cs/lab1/World.java
@@ -0,0 +1,12 @@
+package agh.cs.lab1;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        run();
+    }
+
+    private static void run() {
+        System.out.println("TEST");
+    }
+}
---
+++ src/agh/cs/lab2/World.java
@@ -0,0 +1,7 @@
+package agh.cs.lab2;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
